48cbecf7df82481a2a50e804ad5f2f4f28241125
Rob Redpath <rob@digital-saints.com>
2011-04-11 15:54:16 +0100

    Environment setup - db, etc

---
+++ environ.pm
@@ -0,0 +1,26 @@
+# Get conf
+
+open CONF, "../gb_talks.conf" or die $!;
+my $conf = {};
+while(<CONF>)
+{
+        my @conf_line = split(/=/,$_);
+        chomp $conf_line[1];
+        $conf->{$conf_line[0]} = $conf_line[1];
+}
+
+
+
+# Set up variables, do initial processing
+
+my $sth;
+my $sql;
+my $db_name = $conf->{'mysql_db'};
+my $db_host = $conf->{'mysql_host'};
+my $db_port = $conf->{'mysql_port'};
+
+my $dsn = "dbi:mysql:$db_name:$db_host:$db_port";
+my $db_user = $conf->{'mysql_user'};
+my $db_password =  $conf->{'mysql_pass'};
+
+my $dbh = DBI->connect($dsn, $db_user, $db_password, { RaiseError => 1, AutoCommit => 1 });
