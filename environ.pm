d853a62807309ecc75ef22d99729293453f334f4
Rob Redpath <rob@robredpath.co.uk>
2012-07-02 13:30:46 -0400

    Add initial support for sqlite

--- environ.pm
+++ environ.pm
@@ -15,12 +15,17 @@ while(<CONF>)
 
 # Set up variables, do initial processing
 
-my $db_name = $conf->{'mysql_db'};
-my $db_host = $conf->{'mysql_host'};
-my $db_port = $conf->{'mysql_port'};
+my $db_engine = $conf->{'db_engine'};
+my $db_name = $conf->{'db_name'};
 
-my $dsn = "dbi:mysql:$db_name:$db_host:$db_port";
-my $db_user = $conf->{'mysql_user'};
-my $db_password =  $conf->{'mysql_pass'};
+if ( $db_engine eq "mysql" ) {
+	my $db_host = $conf->{'mysql_host'};
+	my $db_port = $conf->{'mysql_port'};
+	my $dsn = "dbi:mysql:$db_name:$db_host:$db_port";
+	my $db_user = $conf->{'mysql_user'};
+	my $db_password =  $conf->{'mysql_pass'};
+} elsif ( $db_engine eq "sqlite" ) {
+	my $dsn = "dbi:SQLite:dbname=../gb_talks_db.sqlite";
+}
 
 our $dbh = DBI->connect($dsn, $db_user, $db_password, { RaiseError => 1, AutoCommit => 1 });
