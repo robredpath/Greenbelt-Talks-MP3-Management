d6eed4e57b5d70bb208104f72b928d37df8e904b
Rob Redpath <rob@robredpath.co.uk>
2012-07-03 18:01:38 -0400

    Add sample data - should work on both DBMSes

---
+++ setup_files/sample_data.sql
@@ -0,0 +1,25 @@
+INSERT INTO `talks` VALUES (
+(NULL, 2012, "Mr E Xample", "An Awesome Talk", 1),
+(NULL, 2012, "Mr E Xample", "Another Awesome Talk", 1),
+(NULL, 2012, "Mr E Xample", "A Talk That You Can't Have Yet", 0),
+(NULL, 2012, "Ms S Peaker", "A Talk About Something", 0),
+(NULL, 2011, "Mr E Xample", "An Awesome Talk from 2011", 1),
+(NULL, 2011, "Mrs Ann Other", "A Boring Talk from 2011", 1)
+);
+
+INSERT INTO `orders` VALUES (
+
+);
+
+INSERT INTO `order_items` VALUES (
+
+);
+
+INSERT INTO `transcode_queue` VALUES (
+
+);
+
+INSERT INTO `upload_queue` VALUES (
+
+);
+
