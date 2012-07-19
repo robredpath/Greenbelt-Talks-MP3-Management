a29b3abb05bf7771a2bc023b1d87921c99d9b7a0
Rob Redpath <rob@robredpath.co.uk>
2012-07-19 17:39:12 -0400

    A bit more sample data

--- setup_files/sample_data.sql
+++ setup_files/sample_data.sql
@@ -1,10 +1,10 @@
 INSERT INTO `talks` VALUES (
-(NULL, 2012, "Mr E Xample", "An Awesome Talk", 1),
-(NULL, 2012, "Mr E Xample", "Another Awesome Talk", 1),
-(NULL, 2012, "Mr E Xample", "A Talk That You Can't Have Yet", 0),
-(NULL, 2012, "Ms S Peaker", "A Talk About Something", 0),
-(NULL, 2011, "Mr E Xample", "An Awesome Talk from 2011", 1),
-(NULL, 2011, "Mrs Ann Other", "A Boring Talk from 2011", 1)
+(1, 2012, "Mr E Xample", "An Awesome Talk", 1),
+(2, 2012, "Mr E Xample", "Another Awesome Talk", 1),
+(3, 2012, "Mr E Xample", "A Talk That You Can't Have Yet", 0),
+(95, 2012, "Ms S Peaker", "A Talk About Something", 0),
+(108, 2011, "Mr E Xample", "An Awesome Talk from 2011", 1),
+(25, 2011, "Mrs Ann Other", "A Boring Talk from 2011", 1)
 );
 
 INSERT INTO `orders` VALUES (
